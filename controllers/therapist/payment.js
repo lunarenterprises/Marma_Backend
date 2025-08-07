51f66350a5c272d9115c99058e151f1ddc0b09d0
Rahil666 <rahilramesh666@gmail.com>
2025-08-07 16:06:50 +0530

    test api key payment

--- controllers/therapist/payment.js
+++ controllers/therapist/payment.js
@@ -98,13 +98,13 @@ module.exports.Payment = async (req, res) => {
         // Razorpay credentials
         // test api key
 
-        // const key_id = process.env.TEST_KEY_ID;
-        // const key_secret = process.env.TEST_KEY_SECRET;
+        const key_id = process.env.TEST_KEY_ID;
+        const key_secret = process.env.TEST_KEY_SECRET;
 
         //live api keys
 
-        const key_id = process.env.KEY_ID;
-        const key_secret = process.env.KEY_SECRET;
+        // const key_id = process.env.KEY_ID;
+        // const key_secret = process.env.KEY_SECRET;
 
         const callback_url = `https://lunarsenterprises.com:6030/api/therapist/razorpay/callback?payment_id=${payment_id}`;
 
