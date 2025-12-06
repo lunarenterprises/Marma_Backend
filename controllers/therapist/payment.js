188f8b150dbde77c4038eadc5f22745f33fabd0a
Rahil666 <rahilramesh666@gmail.com>
2025-12-06 15:10:10 +0530

    rzp test

--- controllers/therapist/payment.js
+++ controllers/therapist/payment.js
@@ -135,13 +135,13 @@ module.exports.Payment = async (req, res) => {
 
         // test api key
 
-        // const key_id = process.env.TEST_KEY_ID;
-        // const key_secret = process.env.TEST_KEY_SECRET;
+        const key_id = process.env.TEST_KEY_ID;
+        const key_secret = process.env.TEST_KEY_SECRET;
 
         //live api keys
 
-        const key_id = process.env.KEY_ID;
-        const key_secret = process.env.KEY_SECRET;
+        // const key_id = process.env.KEY_ID;
+        // const key_secret = process.env.KEY_SECRET;
 
         const callback_url = `https://www.marma.lunarenp.com/api/therapist/razorpay/callback?payment_id=${payment_id}`;
 
